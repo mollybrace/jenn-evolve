311d57336f17d687a1f9ef93dcc7261e7ee666c6
Molly Brace <molly.rose.brace@gmail.com>
2024-03-10 21:10:46 +0530

    Adding types for build

--- app/components/FeedbackCard.tsx
+++ app/components/FeedbackCard.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+interface FeedbackCardProps {
+  text: string;
+  name: string;
+  country: string;
+}
 
-const FeedbackCard = ({ text, name, country }) => (
+const FeedbackCard: React.FC<FeedbackCardProps> = ({ text, name, country }) => (
     <div className="flex justify-between  flex-col  px-10 py-12 rounded-[20px]  max-w-[370px]  mx-auto my-0 feedback-card bg-khaki-green">
      
       <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-khaki my-10">
